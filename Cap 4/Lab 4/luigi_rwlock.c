108b1b1c07f65caba6619da2f6e8b4e364b1cbdc
luigi <90881256+luigi-rangel@users.noreply.github.com>
2023-09-27 01:19:25 -0300

    Update luigi_rwlock.c

--- Cap 4/Lab 4/luigi_rwlock.c
+++ Cap 4/Lab 4/luigi_rwlock.c
@@ -4,13 +4,15 @@
 
 #include "luigi_rwlock.h"
 
+//criando variaveis globais
 pthread_cond_t rcond, wcond;
 int w = 0, r = 0;
 
+//lock para leitura
 void r_lock(pthread_mutex_t *mutex)
 {
     pthread_mutex_lock(mutex);
-    while (w != 0)
+    while (w != 0) //espera não haver escrita
     {
         pthread_cond_wait(&rcond, mutex);
     }
@@ -22,14 +24,14 @@ void r_unlock(pthread_mutex_t *mutex)
 {
     pthread_mutex_lock(mutex);
     r--;
-    if (r == 0) pthread_cond_signal(&wcond);
+    if (r == 0) pthread_cond_signal(&wcond); //libera a fila de escrita se não houver nenhuma Thread lendo
     pthread_mutex_unlock(mutex);
 }
 
 void w_lock(pthread_mutex_t *mutex)
 {
     pthread_mutex_lock(mutex);
-    while (r != 0 || w != 0)
+    while (r != 0 || w != 0) //espera que não haja nenhuma Thread escrevendo ou lendo
     {
         pthread_cond_wait(&wcond, mutex);
     }
@@ -41,16 +43,18 @@ void w_unlock(pthread_mutex_t *mutex)
 {
     pthread_mutex_lock(mutex);
     w--;
-    pthread_cond_broadcast(&rcond);
-    pthread_cond_signal(&wcond);
+    pthread_cond_broadcast(&rcond); //libera todas as leituras
+    pthread_cond_signal(&wcond); //libera uma Thread na fila para escrita
     pthread_mutex_unlock(mutex);
 }
 
+//inicializa variaveis globais
 void rw_init() {
     pthread_cond_init(&rcond, NULL);
     pthread_cond_init(&wcond, NULL);
 }
 
+//destroi variaveis globais
 void rw_destroy() {
     pthread_cond_destroy(&rcond);
     pthread_cond_destroy(&wcond);
